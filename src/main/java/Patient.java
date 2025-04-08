8ee7a8fe1e8f24b97268e89c7b923ee649e68a4c
BARTLOMIEJ DRAPINSKI- <g00428194@atu.ie>
2025-04-08 09:50:53 +0100

    Working on test failure cases

--- src/main/java/Patient.java
+++ src/main/java/Patient.java
@@ -1,10 +1,45 @@
 public class Patient {
 
     public String givenName;
-
     public String familyName;
     public String HSEnum;
     public int age;
 
+    public String getGivenName() {
+        return givenName;
+    }
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+    public String getFamilyName() {
+        return familyName;
+    }
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+    public String getHSEnum() {
+        return HSEnum;
+    }
+    public void setHSEnum(String HSEnum) {
+        this.HSEnum = HSEnum;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Patient(String givenName, String familyName, String HSEnum, int age) {
+        this.givenName = givenName;
+        this.familyName = familyName;
+        this.HSEnum = HSEnum;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
 
 }
--- src/test/java/testPatient.java
+++ src/test/java/testPatient.java
@@ -2,28 +2,52 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class testPatient {
     Patient myP;
     @BeforeEach
     void setup() {
-        myP = new Patient ();
+        Patient myP;
     }
     @Test
     void testGivenNameSuccess() {
         assertEquals("Bartek", myP.givenName);
     }
     @Test
+    void testGivenNameFailure() {
+       Exception ex = assertThrows(IllegalArgumentException.class, ()-> {});
+       assertEquals("Minimum 3 characters required", ex.getMessage());
+    }
+
+    @Test
     void testFamilyNameSuccess() {
         assertEquals("Drapinski", myP.familyName);
     }
+    @Test
+    void testFamilyNameFailure() {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()-> {});
+        assertEquals("Minimum 3 characters required", ex.getMessage());
+    }
+
     @Test
     void testHSEnumSuccess() {
         assertEquals("4281940000", myP.HSEnum);
     }
     @Test
+    void testHSEnumFailure() {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()-> {});
+        assertEquals("Must be exactly 10 numeric characters", ex.getMessage());
+    }
+
+    @Test
     void testAgeSuccess() {
         assertEquals(20, myP.age);
+    }
+    @Test
+    void testAgeFailure() {
+        Exception ex =assertThrows(IllegalArgumentException.class, ()-> {});
+        assertEquals("Must be between 0 and 120", ex.getMessage());
     }
 
 }
